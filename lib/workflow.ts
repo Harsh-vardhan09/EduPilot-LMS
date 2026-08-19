9f77cb8244e98e32e031f580f6cbe646ec81724a
Harsh <harsh.vardhanp0901@gmail.com>
2026-08-19 23:33:38 +0530

    resend

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,4 +1,4 @@
-import { Client as WorkflowClient } from '@upstash/workflow';
+import { Client as WorkflowClient, WorkflowContext } from '@upstash/workflow';
 import config from './config';
 import { serve } from '@upstash/workflow/nextjs';
 
@@ -7,22 +7,25 @@ export const workflowClient = new WorkflowClient({
   token: config.env.upstash.qstashToken,
 });
 
-export const sendEmail = async ({ email, subject, message }:{email:string,subject:string,message:string}) => {
-  serve(
-    // 👇 the workflow context
-    async (context) => {
-      const { status, body } = await context.api.resend.call('Call Resend', {
-        token: process.env.RESEND_TOKEN!,
-        body: {
-          from: 'EduPilor <onboarding@resend.dev>',
-          to: [email],
-          subject,
-          html: message,
-        },
-        headers: {
-          'content-type': 'application/json',
-        },
-      });
-    }
-  );
+export const sendEmail = async (
+  context: WorkflowContext,
+  {
+    email,
+    subject,
+    message,
+  }: { email: string; subject: string; message: string }
+) => {
+  // 👇 the workflow context
+  await context.api.resend.call('Call Resend', {
+    token: process.env.RESEND_TOKEN!,
+    body: {
+      from: 'EduPilor <onboarding@resend.dev>',
+      to: [email],
+      subject,
+      html: message,
+    },
+    headers: {
+      'content-type': 'application/json',
+    },
+  });
 };
