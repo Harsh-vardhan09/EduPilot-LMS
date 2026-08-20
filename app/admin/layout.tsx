2e85ee48e2768b64180f801c529e29fb31913449
Harsh <harsh.vardhanp0901@gmail.com>
2026-08-20 05:58:15 +0530

    feat:admin layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,6 +2,8 @@ import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
 import { ReactNode } from 'react';
 import '@/styles/admin.css'; 
+import Sidebar from '@/components/admin/Sidebar';
+import Header from '@/components/admin/Header';
 
 const layout = async({children}:{children:ReactNode}) => {
   const session=await auth();
@@ -9,10 +11,10 @@ const layout = async({children}:{children:ReactNode}) => {
 
     return (
     <main className="flex min-h-screen w-full flex-row">
-      <p>Sidebar</p>
+      <Sidebar session={session}/>
 
       <div className='admin-container'>
-        <p>Header</p>
+        <Header session={session}/>
         {children}
       </div>
     </main>
